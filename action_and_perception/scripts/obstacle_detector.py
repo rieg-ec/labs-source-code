fb9f39b40219dd3751cf3fff2ce37dcb7e6b6f33
rieg-ec <rieg@uc.cl>
2021-04-21 23:58:15 +0000

    empty obstacle_detector

---
+++ action_and_perception/scripts/obstacle_detector.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+
+class ObstacleDetector:
+    def __init__(self):
+        self.occupancy_state_publisher = rospy.Publisher(
+            '/occupancy_state', String, queue_size=10)
+
+    def publish_occupancy(self) -> None:
+        rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            self.occupancy_state_publisher.publish("free")
+            rate.sleep()
+
+
+if __name__ == '__main__':
+    rospy.init_node('obstacle_detector', anonymous=True)
+    dead_reck_nav = ObstacleDetector()
+    rospy.spin()
