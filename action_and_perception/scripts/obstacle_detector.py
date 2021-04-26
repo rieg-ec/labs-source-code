a451be5d387faf3825a46f09b038fe727230f687
rieg-ec <rieg@uc.cl>
2021-04-26 00:38:03 -0400

    add sound_play

--- action_and_perception/scripts/obstacle_detector.py
+++ action_and_perception/scripts/obstacle_detector.py
@@ -4,19 +4,41 @@ import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from sound_play.libsoundplay import SoundClient
 import numpy as np
 
 
 class ObstacleDetector:
+
+    sound_messages = {
+        'obstacle_center': "obstacle center",
+        'obstacle_left': "obstacle left",
+        'obstacle_right': "obstacle right",
+    }
+
     def __init__(self) -> None:
         self.camera_suscriber = rospy.Subscriber(
-            '/camera/depth/image_raw', Image, self.image_process)
+            '/camera/depth/image_raw', Image, self._image_process)
         self.occupancy_state_publisher = rospy.Publisher(
             '/occupancy_state', String, queue_size=1)
-        self.obstacle_pos: str = "free"
-        self.bridge = CvBridge()
 
-    def image_process(self, image: Image) -> None:
+        self.bridge = CvBridge()
+        self._obstacle_pos: str = "free"
+        self.soundhandle = SoundClient(blocking=True)
+
+    @property
+    def obstacle_pos(self) -> str:
+        return self._obstacle_pos
+
+    @obstacle_pos.setter
+    def obstacle_pos(self, value: str) -> None:
+        if value != "free":
+            self.soundhandle.say(self.sound_messages[value],
+                                 voice='voice_kal_diphone')
+
+        self._obstacle_pos = value
+
+    def _image_process(self, image: Image) -> None:
         # Image dimension is 480 (fila), 640 (columnas)
         # With self.bridge we convert the depth image to a matrix.
         distance = 2
@@ -53,6 +75,8 @@ class ObstacleDetector:
 
 if __name__ == '__main__':
     rospy.init_node('obstacle_detector', anonymous=True)
+    rospy.init_node('turtlebot_audio')
+
     obstacle_detector = ObstacleDetector()
     obstacle_detector.publish_occupancy()
     rospy.spin()
