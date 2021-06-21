e98184ade78de29027560e1a993d0db0b0de65ee
Benjamin Escobar Blanc <benja.escobar.b@gmail.com>
2021-06-21 13:34:31 -0400

    feat(localization): directories and pkgs created

---
+++ localization/scripts/llc.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+
+import rospy
+import math
+from std_msgs.msg import String, Float64
+from geometry_msgs.msg import Twist, Pose, PoseArray
+from nav_msgs.msg import Odometry
+from typing import Tuple, List
+from utils import ang_error, plot_path, orientation_to_yaw, box_mean_error
+
+
+class NavPID:
+
+    def __init__(self) -> None:
+        rospy.Subscriber('/goal_list', PoseArray, self._move_action_cb)
+        rospy.Subscriber('/odom', Odometry, self._odom_listener)
+        rospy.Subscriber('/occupancy_state', String,
+                         lambda state: setattr(self, 'occupancy_state', state.data))
+
+        # PID
+
+        rospy.Subscriber('/pid_linear/control_effort', Float64,
+                         lambda v: setattr(self, 'lin_speed', v.data * -1))
+
+        rospy.Subscriber('/pid_angular/control_effort', Float64,
+                         lambda v: setattr(self, 'ang_speed', v.data * -1))
+
+        self.velocity_publisher = rospy.Publisher(
+            '/yocs_cmd_vel_mux/input/navigation', Twist, queue_size=1)
+
+        self.ang_setpoint_publisher = rospy.Publisher(
+            '/pid_angular/setpoint', Float64, queue_size=1)
+
+        self.ang_state_publisher = rospy.Publisher(
+            '/pid_angular/state', Float64, queue_size=1)
+
+        self.linear_setpoint_publisher = rospy.Publisher(
+            '/pid_linear/setpoint', Float64, queue_size=1)
+
+        self.linear_state_publisher = rospy.Publisher(
+            '/pid_linear/state', Float64, queue_size=1)
+        # PID
+
+        self.odom_position = Pose()
+
+        self.odom_positions: List[Tuple[float, float]] = []
+
+        self.lin_speed: float = 0
+        self.ang_speed: float = 0
+
+        self.occupancy_state: str = "free"
+
+        self.ERROR_TOLERANCE = 0.01
+
+    def _controlled_forward(self, goal_pos_lin: float, axis: str) -> None:
+        def get_state() -> float:
+            if axis == 'y':
+                return self.odom_position.position.y
+            else:
+                return self.odom_position.position.x
+
+        self.linear_setpoint_publisher.publish(0)
+
+        while self.ERROR_TOLERANCE < abs(get_state() - goal_pos_lin):
+            self.linear_state_publisher.publish(
+                abs(get_state() - goal_pos_lin)
+            )
+
+            velocity = Twist()
+            velocity.linear.x = self.lin_speed
+
+            if self.occupancy_state == 'free':
+                self.velocity_publisher.publish(velocity)
+
+            rospy.Rate(10).sleep()
+
+    def _controlled_turning(self, goal_ang: float) -> None:
+        def get_state() -> float:
+            current_ang = orientation_to_yaw(self.odom_position.orientation)
+            return ang_error(current_ang, goal_ang)
+
+        self.ang_setpoint_publisher.publish(0)
+
+        while self.ERROR_TOLERANCE < abs(get_state()):
+            velocity = Twist()
+            velocity.angular.z = self.ang_speed
+            self.ang_state_publisher.publish(
+                get_state()
+            )
+
+            velocity = Twist()
+            velocity.angular.z = self.ang_speed
+
+            self.velocity_publisher.publish(velocity)
+
+            rospy.Rate(10).sleep()
+
+    def _move_robot_to_destination_ctrl(self, goal_pose: Pose) -> None:
+        dist_y = goal_pose.position.y - self.odom_position.position.y
+        dist_x = goal_pose.position.x - self.odom_position.position.x
+
+        if self.ERROR_TOLERANCE < abs(dist_y):
+            if dist_y > 0:
+                self._controlled_turning(math.pi/2)
+            else:
+                self._controlled_turning(-math.pi/2)
+
+            self._controlled_forward(goal_pose.position.y, axis='y')
+
+        if self.ERROR_TOLERANCE < abs(dist_x):
+            if dist_x > 0:
+                self._controlled_turning(0)
+            else:
+                self._controlled_turning(math.pi)
+
+            self._controlled_forward(goal_pose.position.x, axis='x')
+
+        self._controlled_turning(orientation_to_yaw(goal_pose.orientation))
+
+    def _move_action_cb(self, pose_array: PoseArray) -> None:
+        self.log_poses()
+        for goal_pose in pose_array.poses:
+            self._move_robot_to_destination_ctrl(goal_pose)
+            self.log_poses()
+
+        plot_path(self.odom_positions,
+                  [(i.position.x, i.position.y) for i in pose_array.poses],
+                  box_mean_error(self.odom_positions))
+
+    def _odom_listener(self, odom: Odometry) -> None:
+        self.odom_position = odom.pose.pose
+
+    def log_poses(self):
+        self.odom_positions.append((
+            self.odom_position.position.x,
+            self.odom_position.position.y
+        ))
+
+
+if __name__ == '__main__':
+    rospy.init_node('pid_navigation', anonymous=True)
+    dead_reck_nav = NavPID()
+    rospy.spin()
---
+++ localization/scripts/obstacle_detector.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String, Float64
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge
+from sound_play.libsoundplay import SoundClient
+import numpy as np
+
+
+class ObstacleDetector:
+
+    sound_messages = {
+        'obstacle_center': "obstacle center",
+        'obstacle_left': "obstacle left",
+        'obstacle_right': "obstacle right",
+    }
+
+    def __init__(self) -> None:
+        self.camera_suscriber = rospy.Subscriber(
+            '/camera/depth/image_raw', Image, self._image_process)
+        self.occupancy_state_publisher = rospy.Publisher(
+            '/occupancy_state', String, queue_size=1)
+
+        self.accurate_occupancy_state_publisher = rospy.Publisher(
+            '/wall_distance', Float64, queue_size=1)
+
+        self.bridge = CvBridge()
+        self._obstacle_pos: str = "free"
+        self.soundhandle = SoundClient()
+
+    @property
+    def obstacle_pos(self) -> str:
+        return self._obstacle_pos
+
+    @obstacle_pos.setter
+    def obstacle_pos(self, value: str) -> None:
+        if value != "free" and value != self._obstacle_pos:
+            self.soundhandle.say(self.sound_messages[value],
+                                 voice='voice_kal_diphone')
+
+        self._obstacle_pos = value
+
+    def _image_process(self, image: Image) -> None:
+        # Image dimension is 480 (fila), 640 (columnas)
+        # With self.bridge we convert the depth image to a matrix.
+
+        self.current_image = np.asarray(
+            self.bridge.imgmsg_to_cv2(image, '32FC1'))
+        self.current_image_left = self.current_image[:, :210]
+        self.current_image_center = self.current_image[:, 210:420]
+        self.current_image_right = self.current_image[:, 420:640]
+
+        ## CONTROL PASILLO ##
+        self.wall_distance_publish()
+
+        ## REPORTE OBSTACULOS ##
+        self.obstacle_changes()
+
+    def obstacle_changes(self):
+        distance = 1.2 #
+        # distance = 5 # For hall_navigation.
+        if np.mean(np.mean(self.current_image_center)) < distance:
+            # we take the mean of the mean
+            # because the mean of a matrix is a vector
+            # and the mean of a vector is a number.
+            self.obstacle_pos = "obstacle_center"
+
+        elif np.mean(np.mean(self.current_image_left)) < distance:
+            self.obstacle_pos = "obstacle_left"
+
+        elif np.mean(np.mean(self.current_image_right)) < distance:
+            self.obstacle_pos = "obstacle_right"
+
+        elif (np.mean(np.mean(self.current_image_left)) >= distance and
+              np.mean(np.mean(self.current_image_center)) >= distance and
+              np.mean(np.mean(self.current_image_center)) >= distance
+              ):
+            self.obstacle_pos = "free"
+
+    def wall_distance_publish(self):
+        if self.obstacle_pos == "free":
+            return
+        if np.isnan(np.mean(np.mean(self.current_image_left))):
+            self.accurate_occupancy_state_publisher.publish(
+                Float64(3.0)
+            )
+
+        elif np.isnan(np.mean(np.mean(self.current_image_right))):
+            self.accurate_occupancy_state_publisher.publish(
+                Float64(-3.0)
+            )
+
+        else:
+            diferencia = Float64(np.mean(
+                np.mean(self.current_image_right)) - np.mean(np.mean(self.current_image_left)))
+            self.accurate_occupancy_state_publisher.publish(
+                diferencia
+            )
+
+    def publish_occupancy(self) -> None:
+        while not rospy.is_shutdown():
+            self.occupancy_state_publisher.publish(self.obstacle_pos)
+            rospy.Rate(5).sleep()
+
+
+if __name__ == '__main__':
+    rospy.init_node('obstacle_detector', anonymous=True)
+
+    obstacle_detector = ObstacleDetector()
+    obstacle_detector.publish_occupancy()
+    rospy.spin()
